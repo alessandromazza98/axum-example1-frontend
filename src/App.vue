871ebbb7920f61d84626b5145a70c2194af81205
alessandromazza <alessandro98.mazza@gmail.com>
2023-10-14 17:28:31 +0200

    add frontend

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import useTodos from './components/useTodos.vue';
+const { addTodo, deleteTodo, todos, toggleCompleted } = useTodos();
+
+const newTodoTitle = ref('');
+
+function onInput(e: KeyboardEvent) {
+    if (e.key === 'Enter') {
+        addAndClear()
+    }
+}
+
+async function addAndClear() {
+    await addTodo(newTodoTitle.value);
+    newTodoTitle.value = '';
+}
+</script>
+
+<template>
+    <h2>Todo</h2>
+    <ul class="todo-list">
+        <li v-for="todo in todos" :key="todo.id">
+            <label>
+                <input type="checkbox" :checked="todo.completed" @change="toggleCompleted(todo)" />
+                {{ todo.title }} {{ todo.completed ? '✅' : '' }}
+            </label>
+            <button @click="deleteTodo(todo)">Delete</button>
+        </li>
+    </ul>
+    <input type="text" v-model="newTodoTitle" @keypress="onInput" />
+    <button @click="addAndClear()">Add todo</button>
+</template>
+
+<style>
+.todo-list {
+    list-style: none;
+    padding: 0;
+    text-align: left;
+}
+
+li {
+    display: flex;
+    justify-content: space-between;
+}
+</style>
